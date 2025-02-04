82bae717be04ac1720489f6187e4328676bffede
Mr Robot <hemdan219@gmail.com>
2025-02-04 10:46:23 +0200

    add loading component, cabins list, and enhance cabin pages with image support

---
+++ app/cabins/loading.js
@@ -0,0 +1,12 @@
+import Spinner from "../_components/Spinner";
+
+const loading = () => {
+  return (
+    <>
+      <Spinner />
+      {/* This is test */}
+    </>
+  );
+};
+
+export default loading;
