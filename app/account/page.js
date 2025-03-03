f03db2e78c32a1941369a8a03b9ea3d15b02ed1d
Mr Robot <hemdan219@gmail.com>
2025-03-03 02:58:54 +0200

    implement sign-in and sign-out actions; update SignInButton and SignOutButton components to use forms, add login page, and configure authentication redirection

--- app/account/page.js
+++ app/account/page.js
@@ -1,5 +1,12 @@
-const page = () => {
-  return <h1>This is the guest area account </h1>;
+import { auth } from "../_lib/auth";
+
+const page = async () => {
+  const { user } = await auth();
+  return (
+    <h1>
+      Hello, <span className="font-bold text-accent-600"> {user.name}</span>
+    </h1>
+  );
 };
 
 export default page;
